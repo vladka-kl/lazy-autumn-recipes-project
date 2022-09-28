82ee314ab8d3f5af72eadd322dbe134bf5099f84
VLADYSLAVA KLOVAK <vladaklovak@gmail.com>
2022-09-28 11:45:54 +0200

    navbar added

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,19 +1,9 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function handleMenuDropdown(element) {
+  element.classList.toggle("change");
+  const courses = document.getElementById("menu_id");
+  if (courses.style.display == "block") {
+    courses.style.display = "none";
+  } else {
+    courses.style.display = "block";
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementById("myDropdown");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+}
